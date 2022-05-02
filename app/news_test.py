d5569c48a4c2dd8ac5f4159e734d747958e543ff
Valarie Chelagat <valarie.chelagat@student.moringaschool.com>
2022-05-02 18:00:03 -0400

    created models folder and news.py file

---
+++ app/news_test.py
@@ -0,0 +1,22 @@
+import unittest
+from models import news
+
+News = news.News
+
+class NewsTest(unittest.TestCase):
+    '''
+    Test Class to test the behaviour of the News class
+    '''
+
+    def setUp(self):
+        '''
+        Set up method that will run before every Test
+        '''
+        self.new_news = News('bbc-news','BBC-NEWS','BBC NEWS' 'South Africas Cyril Ramaphosa abandons May Day rally after booing','President Ramaphosa was forced to leave a May Day event after angry workers stormed the stage.','http://www.bbc.co.uk/news/world-africa-61296810','https://ichef.bbci.co.uk/news/1024/branded_news/74A2/production/_124385892_gettyimages-1240390485.jpg','2022-05-02T13:52:23.7862695Z','Image source, Getty Images\r\nImage caption, President Ramaphosa - pictured here at an election victory rally in 2019 - tried to calm down the angry miners to no avail\r\nSouth Africas President Cyril R… [+2721 chars]')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_news,News))
+
+
+if __name__ == '__main__':
+    unittest.main()
