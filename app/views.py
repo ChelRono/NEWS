147a5b1a0f3396f3b64fa69fa26297d824b352a4
Valarie Chelagat <valarie.chelagat@student.moringaschool.com>
2022-05-02 16:17:13 -0400

    made a few changes to views

--- app/views.py
+++ app/views.py
@@ -11,3 +11,11 @@ def index():
 
     message = 'Hello World'
     return render_template('index.html',message = message)
+
+@app.route('/news/<news_id>')
+def movie(news_id):
+
+    '''
+    View movie page function that returns the news details page and its data
+    '''
+    return render_template('news.html',id = news_id)
